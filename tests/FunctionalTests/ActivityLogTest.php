2508bda03504ea0e9136874f6920b6df55699725
Paula <paula.cucuk@gmail.com>
2023-06-07 15:17:50 +0200

    comparing changes test

--- tests/FunctionalTests/ActivityLogTest.php
+++ tests/FunctionalTests/ActivityLogTest.php
@@ -2,12 +2,14 @@
 
 namespace Locastic\Loggastic\Tests\FunctionalTests;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Locastic\Loggastic\Bridge\Elasticsearch\Index\ElasticsearchIndexFactoryInterface;
 use Locastic\Loggastic\DataProvider\ActivityLogProviderInterface;
 use Locastic\Loggastic\Enum\ActivityLogAction;
 use Locastic\Loggastic\Logger\ActivityLoggerInterface;
 use Locastic\Loggastic\Model\ActivityLogInterface;
 use Locastic\Loggastic\Tests\Fixtures\App\Model\DummyBlogPost;
+use Locastic\Loggastic\Tests\Fixtures\App\Model\DummyPhoto;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class ActivityLogTest extends KernelTestCase
@@ -39,25 +41,30 @@ class ActivityLogTest extends KernelTestCase
 
         $activityLogs = $this->activityLogProvider->getActivityLogsByClassAndId(DummyBlogPost::class, 15);
 
-        $editedLog = $activityLogs[0];
         self::assertCount(1, $activityLogs);
-        self::assertInstanceOf(ActivityLogInterface::class, $editedLog);
-        self::assertEquals(ActivityLogAction::CREATED, $editedLog->getAction());
-        self::assertEquals(15, $editedLog->getObjectId());
+
+        $createdLog = $activityLogs[0];
+
+        self::assertInstanceOf(ActivityLogInterface::class, $createdLog);
+        self::assertEquals(ActivityLogAction::CREATED, $createdLog->getAction());
+        self::assertEquals(15, $createdLog->getObjectId());
     }
 
     public function testLogEdit(): void
     {
         $this->blogPost->setTitle('Activity Logs using Elasticsearch');
-        $this->blogPost->setTags(['#php', '#elasticSearch']);
+        $this->blogPost->setTags(['#php', '#locastic', '#elasticSearch']);
         $this->blogPost->setEnabled(true);
+        $this->blogPost->getPhotos()->first()->setPath('https://locastic.com');
 
         $this->activityLogger->logUpdatedItem($this->blogPost);
 
         $activityLogs = $this->activityLogProvider->getActivityLogsByClassAndId(DummyBlogPost::class, 15);
 
-        $editedLog = $activityLogs[1];
         self::assertCount(2, $activityLogs);
+
+        $editedLog = $activityLogs[1];
+
         self::assertInstanceOf(ActivityLogInterface::class, $editedLog);
         self::assertEquals(ActivityLogAction::EDITED, $editedLog->getAction());
         self::assertEquals(15, $editedLog->getObjectId());
@@ -67,11 +74,17 @@ class ActivityLogTest extends KernelTestCase
                 'title' => 'Activity logs in Elasticsearch',
                 'tags' => [1 => '#elasticsearch'],
                 'enabled' => false,
+                'photos' => [
+                    1950 => ['path' => 'path1'],
+                ]
             ],
             'currentValues' => [
                 'title' => 'Activity Logs using Elasticsearch',
-                'tags' => [1 => '#elasticSearch'],
+                'tags' => [1 => '#locastic', 2 => '#elasticSearch'],
                 'enabled' => true,
+                'photos' => [
+                    1950 => ['path' => 'https://locastic.com'],
+                ]
             ],
         ], $editedLog->getDataChangesArray());
     }
@@ -96,6 +109,16 @@ class ActivityLogTest extends KernelTestCase
         $blogPost->setPublishAt(new \DateTime('2022-11-11 15:00:00'));
         $blogPost->setTags(['#php', '#elasticsearch']);
         $blogPost->setTitle('Activity logs in Elasticsearch');
+
+        $photo1 = new DummyPhoto();
+        $photo1->setId(1950);
+        $photo1->setPath('path1');
+
+        $photo2 = new DummyPhoto();
+        $photo2->setId(1911);
+        $photo2->setPath('path2');
+
+        $blogPost->setPhotos(new ArrayCollection([$photo1, $photo2]));
 
         return $blogPost;
     }
