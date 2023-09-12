84cdcda4b93c187e6557f9f60593685e9f2ed5ff
Paula <paula.cucuk@gmail.com>
2023-09-12 11:39:24 +0200

    Use custom dispatcher in CreateActivityLogHandler.php (#3)

--- src/MessageHandler/CreateActivityLogHandler.php
+++ src/MessageHandler/CreateActivityLogHandler.php
@@ -6,9 +6,9 @@ use Locastic\Loggastic\Loggable\LoggableChildInterface;
 use Locastic\Loggastic\DataProcessor\ActivityLogProcessorInterface;
 use Locastic\Loggastic\Message\CreateActivityLogMessageInterface;
 use Locastic\Loggastic\Message\UpdateActivityLogMessage;
+use Locastic\Loggastic\MessageDispatcher\ActivityLogMessageDispatcherInterface;
 use Locastic\Loggastic\Metadata\LoggableContext\Factory\LoggableContextFactoryInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsMessageHandler]
 final class CreateActivityLogHandler
@@ -16,7 +16,7 @@ final class CreateActivityLogHandler
     public function __construct(
         private readonly ActivityLogProcessorInterface $activityLogProcessor,
         private readonly LoggableContextFactoryInterface $loggableContextFactory,
-        private readonly MessageBusInterface $bus
+        private readonly ActivityLogMessageDispatcherInterface $activityLogMessageDispatcher
     ) {
     }
 
@@ -25,7 +25,7 @@ final class CreateActivityLogHandler
         $item = $message->getItem();
 
         if ($item instanceof LoggableChildInterface && is_object($item->logTo())) {
-            $this->bus->dispatch(new UpdateActivityLogMessage($item->logTo()));
+            $this->activityLogMessageDispatcher->dispatch(new UpdateActivityLogMessage($item->logTo()));
         }
 
         $loggableContext = $this->loggableContextFactory->create($message->getClassName());
