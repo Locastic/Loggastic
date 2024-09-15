5f7256dc4f07d95d6005b0b8c845bd4deb2b39f5
Karlo Valentic <karlo.valentic@locastic.com>
2024-09-15 22:16:47 +0200

    Add limit and offset option to ActivityLogProvider

--- src/DataProvider/ActivityLogProviderInterface.php
+++ src/DataProvider/ActivityLogProviderInterface.php
@@ -4,9 +4,9 @@ namespace Locastic\Loggastic\DataProvider;
 
 interface ActivityLogProviderInterface
 {
-    public function getActivityLogsByClass(string $className, array $sort = []): array;
+    public function getActivityLogsByClass(string $className, array $sort = [], int $limit = 20, int $offset = 0): array;
 
-    public function getActivityLogsByClassAndId(string $className, $objectId, array $sort = []): array;
+    public function getActivityLogsByClassAndId(string $className, $objectId, array $sort = [], int $limit = 20, int $offset = 0): array;
 
-    public function getActivityLogsByIndexAndId(string $index, $objectId, array $sort = []): array;
+    public function getActivityLogsByIndexAndId(string $index, $objectId, array $sort = [], int $limit = 20, int $offset = 0): array;
 }
